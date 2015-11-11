0eb3ee7648a54df12c71cc914b1d64b0ea6ca53f
Hinnerk Haardt <haardt@information-control.de>
2015-11-11 11:23:01 +0100

    Get extended information on all nodes in the the parental tree.

--- udev.js
+++ udev.js
@@ -1,11 +1,52 @@
 var udev = require('./build/Release/udev'),
-    EventEmitter = require('events').EventEmitter;
+  EventEmitter = require('events').EventEmitter;
+
+
+var getDetailedNodeChain = function (node) {
+  var result = [], extra;
+  while (node && node.hasOwnProperty('syspath')) {
+    extra = udev.getSysattrBySyspath(node.syspath);
+    for (var key in extra) {
+      if (extra.hasOwnProperty(key)) {
+        node[key] = extra[key];
+      }
+    }
+    result.push(node);
+    node = udev.getNodeParentBySyspath(node.syspath);
+  }
+  return result;
+};
+
+
+var getNodeDetailsSummary = function (node) {
+  var result = {},
+    nodes = getDetailedNodeChain(node),
+    key;
+  while (nodes.length != 0) {
+    node = nodes.pop();
+    for (key in node) {
+      if (node.hasOwnProperty(key)) {
+        if (!result.hasOwnProperty(key)) {
+          result[key] = [];
+        }
+        result[key].push(node[key]);
+      }
+    }
+  }
+  return result;
+};
+
 
 udev.Monitor.prototype.__proto__ = EventEmitter.prototype;
 
+
 module.exports = {
-    monitor: function() { return new udev.Monitor(); },
-    list: udev.list,
-    getNodeParentBySyspath: udev.getNodeParentBySyspath,
-    getSysattrBySyspath: udev.getSysattrBySyspath,
-}
+  monitor: function () {
+    return new udev.Monitor();
+  },
+  list: udev.list,
+  getNodeParentBySyspath: udev.getNodeParentBySyspath,
+  getSysattrBySyspath: udev.getSysattrBySyspath,
+  getNodeChain: getDetailedNodeChain,
+  getNodeDetails: getNodeDetailsSummary
+};
